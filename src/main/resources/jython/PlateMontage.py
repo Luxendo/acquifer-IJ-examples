04437dac30152697b46f2848bd1191cbf6972587
Laurent Thomas <laurent132.thomas@laposte.net>
2022-02-28 13:05:46 +0100

    Update PlateMontage.py

--- src/main/resources/jython/PlateMontage.py
+++ src/main/resources/jython/PlateMontage.py
@@ -4,32 +4,25 @@ The IM directory is expected to contain a dataset with a SINGLE SUBPOSITION
 """
 
 #@ File (label="Select an IM directory", style="directory") image_directory
-from acquifer.core.im04 import FileUtils, Metadata
+from acquifer.core.im04 import FileUtils, DatasetBuilder
 from acquifer.ij.Utils  import printArray
 from acquifer.ij        import VirtualPlate
-from acquifer.core.im   import Plate
+from acquifer.core      import Plate, PlateSeries
 from ij import IJ, ImagePlus
 
-image_directory = image_directory.toString()
-
-listMetadata = FileUtils().getListMetadatas(image_directory) # list IM files and parse dimensions metadatas
-sortedMetadata = Metadata.sortCopy(listMetadata, VirtualPlate.SORT_CZT) 
-printArray(sortedMetadata)
+dataset = DatasetBuilder(image_directory).build()
+plateSeries = PlateSeries.groupByPlateSeries(dataset)[0] # take the first plate series in the directory. There might be multiple plateSeries if there is more than one subposition
 
 layout = Plate.Wells96
 downScaling = 10
 
-
-# Method 1 - Create a VirtualPlate then wrap it into an ImagePlus
-virtualPlate = VirtualPlate(listMetadata, layout, downScaling)
-virtualPlate.deleteSlice(1) # to test
-
-virtualPlateImp = ImagePlus("Method1 - After removing slice 1", virtualPlate)
-virtualPlateImp.show() 
+# Method 1 - Create a VirtualPlate then call toHyperstack
+virtualPlate = VirtualPlate(plateSeries, layout, downScaling)
+hyperstack = virtualPlate.toHyperstack() 
+hyperstack.show()
 
 # Method 2 : directly create a hyperstack
-plateStack = VirtualPlate.createHyperstackFromDataset(listMetadata,
-													  layout,
-													  downScaling)
-imp2 = plateStack.toHyperstack()
-imp2.show() 
+hyperstack2 = VirtualPlate.createHyperstackFromPlateSeries(plateSeries,
+													  	  layout,
+													  	  downScaling)
+hyperstack2.show() 

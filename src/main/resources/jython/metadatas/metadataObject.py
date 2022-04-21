65e6770c63c2f83831a7ded4ed59e1d457d18796
Laurent Thomas <laurent132.thomas@laposte.net>
2022-04-21 12:18:57 +0200

    Z position µm

--- src/main/resources/jython/metadatas/metadataObject.py
+++ src/main/resources/jython/metadatas/metadataObject.py
@@ -38,4 +38,4 @@ print m4, "exists:", m4.exists()
 # See the API documentation for a list of available functions
 # https://acquifer.github.io/acquifer-core/acquifer/core/im/Metadata.html
 print "Pixels size (micrometers):", m1.getPixelSize()
-print "Z-Position (mm): ", m1.getPositionZ()
+print "Z-Position (micrometers): ", m1.getPositionZ()

f641c555cff938ffb9e0299cce9e09e9d771650e
Laurent Thomas <laurent132.thomas@laposte.net>
2022-04-26 12:03:42 +0200

    add GUI for output directory acquire demo

--- src/main/resources/jython/TcpIp/7_Acquire.py
+++ src/main/resources/jython/TcpIp/7_Acquire.py
@@ -28,7 +28,10 @@ using the setMode("script") command.
 See the API documentation for the list of commands available by TcpIp and their docstrings. 
 https://acquifer.github.io/acquifer-core/acquifer/core/TcpIp.html
 """
+#@File (style="directory") directory
+
 from acquifer.core import TcpIp
+import os
 
 try : 
 	myIM = TcpIp() # open the communication port with the IM
@@ -37,10 +40,12 @@ except Error, error :
 	IJ.error(error.getMessage())
 	raise Exception(error) # still throw an error to interrupt code execution
 
+directory = directory.getPath()
+
 myIM.resetCamera() # use default setting, use setCamera functions to update ROI and/or binning
 
 # Update metadata fields such as wellID, subposition (within a given well), timepoint
-myIM.setMetadataWellId("C002") # this should be a 4-character string : a character and 3 digits 
+myIM.setMetadataWellId("A001") # this should be a 4-character string : a character and 3 digits 
 myIM.setMetadataSubposition(1) # to specify if we are acquiring subpositions within a well
 myIM.setMetadataTimepoint(1)   # for timelapse
 
@@ -64,8 +69,7 @@ lightConstantOn = False
 # one can also use normal string with double backslashes as separators \\ or forward slash /
 # if the directory does not exist, it is automatically created
 # if "" or None is passed as argument to acquire, the images are saved in the default project folder within a plate-specific directory (see below) 
-saveDirectory = r"C:\Users\Default\Desktop\MyDataset"
-
+saveDirectory = os.path.join(directory, "customDirectory")
 myIM.acquireZstack(channelNumber,
 				 objectiveIndex, 
 				 lightSource, 
@@ -85,8 +89,8 @@ myIM.acquireZstack(channelNumber,
 #
 # This will assure the following acquire commands are saved in the same subdirectory
 # Indeed when switching to script mode, a timestamp is created that is used as part of the plate directory
-myIM.setDefaultProjectFolder(r"C:\Users\Default\Desktop\MyDataset")
-myIM.setPlateId("test")
+myIM.setDefaultProjectFolder(os.path.join(directory, "defaultProjectDirectory" ))
+myIM.setPlateId("myPlate")
 
 # Switch to script mode before calling successive acquire commands
 # otherwise each acquire command will switch back to live mode after execution (time consuming)
@@ -134,3 +138,4 @@ myIM.acquireZstack(2, # here we set channel number for this fluo channel to 2, t
 				 None) # set the saveDirectory to None, in this case the images are saved to the default project directory as above
 
 myIM.closeConnection() # closing the connection will automatically switch back to live mode
+print "Done"

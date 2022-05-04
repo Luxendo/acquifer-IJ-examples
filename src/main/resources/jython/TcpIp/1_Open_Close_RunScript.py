c0d86c948cfe9a4cca6305afe4539949fad8af9b
Laurent Thomas <laurent132.thomas@laposte.net>
2022-05-04 12:18:41 +0200

    add field for input script + sound notification

--- src/main/resources/jython/TcpIp/1_Open_Close_RunScript.py
+++ src/main/resources/jython/TcpIp/1_Open_Close_RunScript.py
@@ -14,8 +14,12 @@ therefore no other command is usually needed in this scenario.
 See the API documentation for the list of commands available by TcpIp and their docstrings. 
 https://acquifer.github.io/acquifer-core/acquifer/core/TcpIp.html
 """
+#@ String (visibility=MESSAGE, value="Clicking OK will open the IM lid, wait for a user-defined time, close the lid and start a pre-defined script.", required=false) msg
+#@ File (style="file") script_path
+#@ Integer (label="Wait for (secs)", value=5) delay
 from acquifer.core import TcpIp
 from java.lang import Thread
+from ij import IJ
 
 try : 
 	myIM = TcpIp() # open the communication port with the IM
@@ -26,17 +30,22 @@ except Error, error :
 
 myIM.openLid() 
 
-# Or commands for a robotic arm to put a new plate
-Thread.sleep(3000) # pause execution for 3 seconds
+Thread.sleep(delay * 1000) # pause execution in milliseconds
+# here could be commands for a robotic arm to put a new plate, pipet new reagents...
 
 myIM.closeLid()
 
 # Here we use a raw-string (prefixed with r), to prevent backslash to be interpreted as special character such as new line for \n
 # You dont need raw string if your path is using forward slash / or double back slash \\ as separator
-experimentPath = r"C:\Users\Admin\Example\myScript.imsf"
-myIM.runScript(experimentPath) # This will pause further code execution until the script is finished running
+myIM.runScript(script_path.getPath()) # This will pause further code execution until the script is finished running
 
 # Just close the port once the script is finished, also switching off any light source if any
 myIM.closeConnection()
-print "Done"
-# You could even put some custom code here to send a mail notification once the experiment is over
+
+# Notify that the script finished
+IJ.beep()    # play a sound notification
+print "Done" # seen in the script editor
+IJ.showStatus("Finished acquisition")
+
+# One could even send a mail notification once the experiment is over
+# but more complicated
